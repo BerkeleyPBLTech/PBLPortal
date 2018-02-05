8a217fa6ad1ad9d5fe4bd578c8165f56b3e56523
Bob Feng <bob.feng.com@gmail.com>
2018-02-05 10:51:26 -0800

    swapping form and also switched yichu and Megan

--- js/main.js
+++ js/main.js
@@ -10,33 +10,26 @@
 					["fri10", "fri11", "fri12", "fri1"]];
 
 	// list of days, then time slots per day, then people per time slot
-	/*[
-					[
-						["Yusuf Yamani", "Eliot Hsu"], 
-						["Donna Ye", "David Pailet", "Zachary Osterland", "Sabrina Ni", "Random Person"],
-						["Ozair Khan", "Tonya Nguyen", "Rena Song", "Meejin Choi"], 
-						["Jessica Tzeng", "Leona Chen", "Tae Kim"]],
-					[
-						["Austin Kim", "Justin Kim", "Caroline Chen"], 
-						["Linda Li", "Cynthia Luan", "Sean Park"],
-						["Hermes Ip", "Sarah Shin", "Paras Dave", "Annie Ouyang"], 
-						["Vanessa Lin", "Wenbo Guo", "Arash Nassirinia"]],
-					[
-						["Taylor Wang", "Bob Feng", "Corey Gibbel", "Hyeonjo Jeong"], 
-						["Emily Zhu", "Jack Jones", "Linda Zheng"],
-						["Nicholas Paul", "Samantha Fang", "Younghoon Oh"], 
-						["Emeri Zhang", "Tia Marie Vo"]],
-					[
-						["Lindsay Yang", "Stephanie Chen", "Emily Jin", "Eric Sung"], 
-						["Coco Jiang", "Sonia Park"],
-						["Shalandy Zhang", "Megan Wang", "Yichu Chen"], 
-						["Alex Park", "Anjanaa Senthil Kumar", "Alice Cho"]],
-					[
-						["Copter Ruangrojanin", "Cyssi Ngo", "Kristy Kao", "Carrington Park"], 
-						["Sunbin Kim", "Nancy Chen", "Chloe Lin", "Howard Yin"],
-						["Arnold Chan", "Mikuto Murayama", "Roy Kamineni", "Josia Yuan"], 
-						["Brian Zeng", "Amy Lu", "Nathan Orwig"]]] */
-	var json = [[["Sean Lee", "Stephanie Chen", "Eliot Hsu", "Yichu Chen"], ["Taylor Wang", "Jacqueline Zhang", "Samantha Sujo", "Michael Hsiu"], ["Hermes Ip", "Leona Chen", "Julian Tedja", "Ravi Siripuram"], ["Emeri Zhang", "Adam Liu", "Lily Zheng", "Shaylene Liao"]], [["Meejin Choi", "Bianca Al-Shamari", "Alex Park", "Annie Cui"], ["Linda Li", "Lorenzo dela Cruz", "Hyeonjo Jeong", "Anny Song"], ["Corey Gibbel", "Rafay Warsi", "Caroline Chen"], ["Sarah Shin", "Arina Yu", "Ozair Khan", "Jessica Dang"]], [["Chloe Lin", "Jeffrey Li", "Nayada (Mint) Katevetin"], ["Nancy Chen", "William Kim", "Kan Liu"], ["Samantha Fang", "Kristina Lee"], ["Kira Guo", "Anastasia Vela", "Troy Lee", "Max Wang"]], [["Bob Feng", "Anthony Chen"], ["Alexander Park", "Arnold Chan", "Claire Lee", "Austin Kim"], ["Carrington Park", "Elizabeth Pegg", "Megan Wang", "Shruti Maheshwari"], ["Sabrina Ni", "Eric Sung", "Rick Surya", "Rena Song"]], [["Tae Kim", "Kai Lun Chen"], ["Nicholas Paul", "Mi (Jackie) Hu"], ["Yusuf Yamani", "Janet Xu", "Nick Palmer"], ["Emily Jin", "Timothy Huang", "Alan Dai"]]];
+
+	var json = [[["Sean Lee", "Stephanie Chen", "Eliot Hsu", "Megan Wang"], 
+	["Taylor Wang", "Jacqueline Zhang", "Samantha Sujo", "Michael Hsiu"], 
+	["Hermes Ip", "Leona Chen", "Julian Tedja", "Ravi Siripuram"], 
+	["Emeri Zhang", "Adam Liu", "Lily Zheng", "Shaylene Liao"]], 
+	[["Meejin Choi", "Bianca Al-Shamari", "Alex Park", "Annie Cui"], 
+	["Linda Li", "Lorenzo dela Cruz", "Hyeonjo Jeong", "Anny Song"], 
+	["Corey Gibbel", "Rafay Warsi", "Caroline Chen"], 
+	["Sarah Shin", "Arina Yu", "Ozair Khan", "Jessica Dang"]], 
+	[["Chloe Lin", "Jeffrey Li", "Nayada (Mint) Katevetin"], 
+	["Nancy Chen", "William Kim", "Kan Liu"], 
+	["Samantha Fang", "Kristina Lee"], 
+	["Kira Guo", "Anastasia Vela", "Troy Lee", "Max Wang"]], 
+	[["Bob Feng", "Anthony Chen"], 
+	["Alexander Park", "Arnold Chan", "Claire Lee", "Austin Kim"], 
+	["Carrington Park", "Elizabeth Pegg", "Yichu Chen", "Shruti Maheshwari"], 
+	["Sabrina Ni", "Eric Sung", "Rick Surya", "Rena Song"]], 
+	[["Tae Kim", "Kai Lun Chen"], ["Nicholas Paul", "Mi (Jackie) Hu"], 
+	["Yusuf Yamani", "Janet Xu", "Nick Palmer"], 
+	["Emily Jin", "Timothy Huang", "Alan Dai"]]];
 
 
 	$.each(json, function(i) { // loops through each day
@@ -57,6 +50,14 @@
 	$(window).on('resize', function() {
 		$('.input_button').attr('href', function() {
 			return hrefs[$(window).width() > 737 ? 0 : 1];
+		});
+	}).trigger('resize');
+
+
+	var hrefxs = [$('.swap_button').attr('href'), 'https://goo.gl/forms/nmjSGBXmHgyE65Rm1'];
+	$(window).on('resize', function() {
+		$('.swap_button').attr('href', function() {
+			return hrefxs[$(window).width() > 737 ? 0 : 1];
 		});
 	}).trigger('resize');
 
